19743a431cd764b6076c7702377017d19554cf4e
Pavel <kurilo.pavel@mail.ru>
2024-04-17 23:38:14 +0300

    add background page

--- web/src/pages/Pagination.tsx
+++ web/src/pages/Pagination.tsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import classNames from "classnames";
 
 type PaginationProps = {
   usersCount: number;
@@ -7,6 +8,7 @@ type PaginationProps = {
 
 const Pagination = ({usersCount, setNewUsers}: PaginationProps) => {
   const [page, setPage] = useState(0);
+  const [currentPage,setCurrentPage] = useState(1)
   const count = 10;
   let arr = []
   for (let i = 1 + page; i <= count + page; ++i) {
@@ -19,6 +21,7 @@ const Pagination = ({usersCount, setNewUsers}: PaginationProps) => {
   };
 
   const getUsers = async (value) => {
+    setCurrentPage(value);
     const response = await fetch(`http://localhost:3000/users/${value}`);
     const data = await response.json();
     setNewUsers(data.users);
@@ -28,7 +31,7 @@ const Pagination = ({usersCount, setNewUsers}: PaginationProps) => {
     <span className="link_page" onClick={() => handleClick(-count)}>&laquo;</span>
     <span className="link_page" onClick={() => handleClick(-1)}>&lsaquo;</span>
     {arr.map((numberPage: number) => {
-      return <span className="link_page" key={numberPage} onClick={() => {
+      return <span className={classNames("link_page",{"current_page": currentPage === numberPage})} key={numberPage} onClick={() => {
         getUsers(numberPage);
       }}
       >{numberPage}</span>
